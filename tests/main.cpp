1116606574fa9a5356d2c8d29264561370a305c9
Ondřej Novák <ondra-novak@email.cz>
2022-08-05 22:04:48 +0200

    testing

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,13 +1,67 @@
+#include <userver/async_provider.h>
+#include <userver/connect.h>
+#include <userver/netaddr.h>
+#include <userver/socket.h>
+#include <userver/stream.h>
 #include "userver/sha1.h"
 #include <iomanip>
 #include <iostream>
+#include <thread>
+
+#include "../publisher.h"
+
+
+void read_cycle(userver::Stream &s) {
+    s.read() >> [&](const std::string_view &c) {
+        if (c.empty()) {
+            if (s.timeouted()) {
+                s.clear_timeout();
+                read_cycle(s);
+            }
+        } else {
+            std::cout << c << std::endl;
+            read_cycle(s);
+        }
+    };
+}
 
 int main(int argv, char **argc) {
-    std::string d = userver::SHA1::hash("test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message test message");
-    for (char c : d) {
-        unsigned char cc = static_cast<unsigned char>(c);
-        std::cout << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(cc);
-    }
-    std::cout << std::endl;
-    return 0;
+    auto addr = userver::NetAddr::fromString("localhost", "10000");
+    auto provider = userver::createAsyncProvider({1,6,false,true});
+    userver::setCurrentAsyncProvider(provider);
+
+    bool exit_cycle = false;
+
+
+    connect(addr) >> [&](std::optional<userver::Stream> &&stream) {
+
+        if (stream.has_value()) {
+           userver::Stream s(std::move(*stream));
+           std::string t;
+           std::size_t n = 0;
+
+           read_cycle(s);
+
+           while (!exit_cycle) {
+                t = std::to_string(n);
+                t.append("\n");
+                s.write(t, true) >> [&](bool x){
+                    if (x == false)
+                        exit_cycle = true;
+                };
+                n++;
+           }
+           std::cout << "Cycle exit" << std::endl;
+        } else {
+            std::cout << "Connect error" << std::endl;
+        }
+
+    };
+
+    std::cout << "Press enter (1)" << std::endl;
+    std::cin.get();
+    exit_cycle = true;
+    std::cout << "Press enter (2)" << std::endl;
+    std::cin.get();
+    provider.stop();
 }
